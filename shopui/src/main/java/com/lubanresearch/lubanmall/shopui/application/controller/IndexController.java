4abfb336207d172845ddaa233569d574e3dec41a
hilbertcao <597895798@qq.com>
2017-11-17 00:51:24 +0800

    注释

--- shopui/src/main/java/com/lubanresearch/lubanmall/shopui/application/controller/IndexController.java
+++ shopui/src/main/java/com/lubanresearch/lubanmall/shopui/application/controller/IndexController.java
@@ -16,6 +16,6 @@ public class IndexController {
     @RequestMapping(path = {"/go","/"})
     @ResponseBody
     public String go() {
-        return "success";
+        return "success!";
     }
 }
