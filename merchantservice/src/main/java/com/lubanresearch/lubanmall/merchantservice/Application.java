b8f5d0798cc111e6af5f17059f2324314cb21eb3
caopf <caopf@glodon.com>
2018-01-03 18:04:08 +0800

    修改mybatis的mapper重复的问题

--- merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
+++ merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
@@ -1,5 +1,6 @@
 package com.lubanresearch.lubanmall.merchantservice;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients
 @SpringBootApplication
 @ComponentScan(basePackages={"com.lubanresearch.lubanmall.merchantservice"})
+@MapperScan("com.lubanresearch.lubanmall.merchantservice.infrastructure.persistence.db")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
