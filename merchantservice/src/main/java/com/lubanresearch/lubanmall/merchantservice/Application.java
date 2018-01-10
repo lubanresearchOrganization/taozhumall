60da79f9f0df24f98e8b64627926a55a2d2187ea
hilbertcao <597895798@qq.com>
2018-01-11 01:31:21 +0800

    修改mybatis扫描接口

--- merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
+++ merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients
 @SpringBootApplication
 @ComponentScan(basePackages={"com.lubanresearch.lubanmall.merchantservice"})
-@MapperScan("com.lubanresearch.lubanmall.merchantservice.infrastructure.persistence.db")
+@MapperScan(basePackages="com.lubanresearch.lubanmall.merchantservice.infrastructure.persistence.db")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
