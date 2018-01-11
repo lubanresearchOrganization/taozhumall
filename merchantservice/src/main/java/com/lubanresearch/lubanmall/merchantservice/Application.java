ea886d6a212cb0694bf342d6ac9d0aa08f4cfed7
caopf <caopf@glodon.com>
2018-01-11 10:26:53 +0800

    修改包扫描的路径

--- merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
+++ merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableFeignClients
 @SpringBootApplication
 @ComponentScan(basePackages={"com.lubanresearch.lubanmall.merchantservice"})
-@MapperScan(basePackages="com.lubanresearch.lubanmall.merchantservice.infrastructure.persistence.db")
+@MapperScan(basePackages="com.lubanresearch.lubanmall.merchantservice.infrastructure.persistence.db.mapper")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
