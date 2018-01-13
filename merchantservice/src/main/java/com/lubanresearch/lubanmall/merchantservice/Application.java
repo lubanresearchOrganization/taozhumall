523e7db3bf24c41dffeb3ec6fbe01aaa95a4f350
hilbertcao <597895798@qq.com>
2018-01-14 02:57:24 +0800

    解决hibernate扫描的问题

--- merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
+++ merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
@@ -3,9 +3,11 @@ package com.lubanresearch.lubanmall.merchantservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author hilbert.cao
@@ -15,6 +17,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages={"com.lubanresearch.lubanmall.merchantservice"})
 @MapperScan(basePackages="com.lubanresearch.lubanmall.merchantservice.infrastructure.persistence.db.mapper")
+@EnableJpaRepositories(basePackages = "com.lubanresearch.lubanmall.merchantservice")
+@EntityScan(basePackages = "com.lubanresearch.lubanmall.merchantservice")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
