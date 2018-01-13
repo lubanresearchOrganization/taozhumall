a94a57080767cdee6ca30b598e83822e15071366
hilbertcao <597895798@qq.com>
2018-01-14 04:06:30 +0800

    解决hibernate扫描的问题

--- merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
+++ merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
@@ -2,12 +2,12 @@ package com.lubanresearch.lubanmall.merchantservice;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author hilbert.cao
@@ -17,8 +17,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @ComponentScan(basePackages={"com.lubanresearch.lubanmall.merchantservice"})
 @MapperScan(basePackages="com.lubanresearch.lubanmall.merchantservice.infrastructure.persistence.db.mapper")
-@EnableJpaRepositories(basePackages = "com.lubanresearch.lubanmall.merchantservice")
-@EntityScan(basePackages = "com.lubanresearch.lubanmall.merchantservice")
+@EnableAutoConfiguration(exclude=HibernateJpaAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
