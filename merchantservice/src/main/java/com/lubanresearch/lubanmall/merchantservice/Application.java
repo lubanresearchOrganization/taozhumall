ea245291595dc4f007ac0b4e3e6d7da0a5bc3ef4
hilbertcao <597895798@qq.com>
2018-01-14 04:49:46 +0800

    解决merchant serivce jpa的问题

--- merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
+++ merchantservice/src/main/java/com/lubanresearch/lubanmall/merchantservice/Application.java
@@ -17,7 +17,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages={"com.lubanresearch.lubanmall.merchantservice"})
 @MapperScan(basePackages="com.lubanresearch.lubanmall.merchantservice.infrastructure.persistence.db.mapper")
-@EnableAutoConfiguration(exclude=HibernateJpaAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
