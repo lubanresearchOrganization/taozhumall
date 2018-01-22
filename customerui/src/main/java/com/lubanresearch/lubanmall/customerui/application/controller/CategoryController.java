d4608d03338bebce97f4c2e27eb415769223db66
hilbertcao <597895798@qq.com>
2018-01-22 21:40:17 +0800

    类目

---
+++ customerui/src/main/java/com/lubanresearch/lubanmall/customerui/application/controller/CategoryController.java
@@ -0,0 +1,32 @@
+package com.lubanresearch.lubanmall.customerui.application.controller;
+
+import com.lubanmall.catagoryserviceapi.bean.CategoryDTO;
+import com.lubanresearch.lubanmall.customerui.infrastructure.remote.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+
+/**
+ * Created by hilbertcao on 2018/1/22.
+ */
+@Controller
+@RequestMapping("/v/0.1/categorys")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public List<CategoryDTO> getCategorys(@RequestParam(value = "parentId",required = false) Long parentId) {
+
+        return categoryService.getCategorys(parentId);
+
+    }
+
+}
---
+++ customerui/src/main/java/com/lubanresearch/lubanmall/customerui/infrastructure/remote/CategoryService.java
@@ -0,0 +1,20 @@
+package com.lubanresearch.lubanmall.customerui.infrastructure.remote;
+
+import com.lubanmall.catagoryserviceapi.bean.CategoryDTO;
+import com.lubanresearch.lubanmall.common.exception.ServiceException;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by hilbertcao on 2018/1/22.
+ */
+@FeignClient(name = "categoryService")
+public interface CategoryService {
+
+
+    @RequestMapping(value = "/v/0.1/categorys/", method = RequestMethod.GET)
+    @ResponseBody
+    public List<CategoryDTO> getCategorys(@RequestParam(value = "parentId",required = false) Long parentId)throws ServiceException;
+}
