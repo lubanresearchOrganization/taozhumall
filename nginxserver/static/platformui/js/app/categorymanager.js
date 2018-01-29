83a07351e4b9f4031858cc674992124febf69e97
zhangyifei <278999401@qq.com>
2018-01-29 20:11:50 +0800

    类目html列表页面

--- nginxserver/static/platformui/js/app/categorymanager.js
+++ nginxserver/static/platformui/js/app/categorymanager.js
@@ -1,43 +1,81 @@
-var categorymanager = (function ($,converter){
+var categorymanager = (function ($, converter) {
 
     var categorymanager = {};
     categorymanager.init = function () {
 
 
+        var zTreeObj;
+        var setting = {
+
+            view: {
+                dblClickExpand: false,
+                showLine: true,
+                selectedMulti: false
+            },
+            data: {
+                simpleData: {
+                    enable: true,
+                    idKey: "id",
+                    pIdKey: "parentId",
+                    rootPId: ""
+                }
+            }
+
+            , callback: {
+                onClick: categorymanager.ztreeClick
+            }
+
+        };
+        var zNodes;
+
+
         $.get({
-            url:config.categoryUrl+"/v/0.1/categorys/",
-            type:'GET',
-            dataType:'JSON',
-            success:function(result){
+            url: config.categoryUrl + "/v/0.1/categorys/",
+            type: 'GET',
+            dataType: 'JSON',
+            success: function (result) {
 
 
-                console.log(result);
 
-                var tree = converter.listToTree(result,{
-                    idKey:"id",
-                    parentKey:"parentId",
-                    childrenKey:"nodes",
-                    nameFromKey:"name",
-                    nameToKey:"text"
-                });
+                zNodes = categorymanager.addParentFlagToTreeData(result);
 
-                console.log(tree);
-                $('#treeDemo').treeview(
-                    {
-                        data:tree,
-                        levels:3
-                    });
-
+                zTreeObj = $.fn.zTree.init($("#tree"), setting, zNodes);
             }
         });
 
     };
 
+    categorymanager.ztreeClick = function (event, treeId, treeNode) {
+
+        alert(treeNode.id);
+
+    }
+
+    categorymanager.addParentFlagToTreeData = function (result) {
+
+        for (var i = 0; i < result.length; i++) {
+
+            var category = result[i];
+
+
+            if (category.parentId == 0) {
+
+                category["isParent"] = true;
+
+            }
+
+        }
+
+        return result;
+
+    }
+
+
     return categorymanager;
 
-})(jQuery,converter,config);
+})(jQuery, converter, config);
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     categorymanager.init();
 });
