6e0dbf5841f19487aaeee822b9347aecc876c381
hilbertcao <597895798@qq.com>
2018-02-20 21:10:19 +0800

    hibernate 在userservice错误处理

--- userservice/src/main/java/com/lubanresearch/lubanmall/userservice/Application.java
+++ userservice/src/main/java/com/lubanresearch/lubanmall/userservice/Application.java
@@ -2,7 +2,9 @@ package com.lubanresearch.lubanmall.userservice;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,6 +17,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages={"com.lubanresearch.lubanmall.userservice"})
 @MapperScan("com.lubanresearch.lubanmall.userservice.infrastructure.persistence.db")
+@EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
