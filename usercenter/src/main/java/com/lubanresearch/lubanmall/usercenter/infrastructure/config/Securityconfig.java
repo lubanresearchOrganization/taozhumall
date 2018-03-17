1cb25c1fb89210438aa259cd01c596169d091484
hilbertcao <597895798@qq.com>
2018-03-18 01:24:38 +0800

    sso跨域

--- usercenter/src/main/java/com/lubanresearch/lubanmall/usercenter/infrastructure/config/Securityconfig.java
+++ usercenter/src/main/java/com/lubanresearch/lubanmall/usercenter/infrastructure/config/Securityconfig.java
@@ -21,7 +21,6 @@ public class Securityconfig {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-        config.addExposedHeader("*");
         config.setAllowCredentials(true); // 允许cookies跨域
         config.addAllowedOrigin("*");// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
         config.addAllowedHeader("*");// #允许访问的头信息,*表示全部
