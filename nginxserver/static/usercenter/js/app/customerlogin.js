a48fe32ff27f98c43add9865664ecf5c9cd8890c
hilbertcao <597895798@qq.com>
2018-03-20 23:26:55 +0800

    ajax登录

--- nginxserver/static/usercenter/js/app/customerlogin.js
+++ nginxserver/static/usercenter/js/app/customerlogin.js
@@ -5,6 +5,19 @@ var customerlogin = (function ($){
 　　　　　　
          $("#submitBtn").on("click",function(){
               $('#loginForm').submit();
+              var formData = {
+                  "name":$("#nameInput").val(),
+                  "password":$("#passwordInput").val()
+              };
+              $.post("usercente.taozhumall.com/ajaxLogin", formData,
+                 function(result){
+                   if(result.code==200){
+                   alert("登录成功!");
+                   window.location.href = result.data;
+                   }else{
+                     alert(result.message);
+                   }
+                 }, "json");
          });
 　　　　};
 
