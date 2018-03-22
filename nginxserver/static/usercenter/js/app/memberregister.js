301044a0b2951d1b69d1c3a657b7d2b625ac8c1b
hilbertcao <597895798@qq.com>
2018-03-23 01:17:13 +0800

    注册功能实现

--- nginxserver/static/usercenter/js/app/memberregister.js
+++ nginxserver/static/usercenter/js/app/memberregister.js
@@ -8,22 +8,18 @@ var memberregister = (function ($){
               var name = $("#nameInput").val();
               var password = $("#passwordInput").val();
               var repassword = $("#repasswordInput").val();
-
               if(!name){
                 alert("请输入用户名!");
                 return false;
               }
-
               if(!password){
                 alert("请输入密码!");
                 return false;
               }
-
               if(!repassword){
                 alert("请确认密码!");
                 return false;
               }
-
               if(repassword != repassword){
                 alert("两次密码不一致，请重新确认!");
                 return false;
@@ -33,19 +29,22 @@ var memberregister = (function ($){
                   "name":name,
                   "password":password
               };
-              $.ajax({
-                   type : "post",
-                   beforeSend: lajax.beforeSend,
-                   url : "/register",
-                   data : JSON.stringify(formData),
-                   contentType : "application/json; charset=utf-8",
-                   dataType : "json",
-                   success : function(result){
-                          alert("注册成功!");
-                          window.location.href = "http://www.taozhumall.com";
-                    }, "json");
-                    }
-                   });
+
+
+               $.ajax({
+                      type : "post",
+                      url : "/register",
+                      data : JSON.stringify(ldata),
+                      contentType : "application/json; charset=utf-8",
+                      dataType : "json",
+                      success : function(result){
+                                 alert("注册成功!");
+                                 window.location.href = "http://www.taozhumall.com";
+                                }
+                     });
+
+
+         });
 　　　　};
 
 　　　　return memberregister;
