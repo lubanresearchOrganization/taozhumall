45537fcfe7fcc9a11f82249fbe36c26aefab66e3
hilbertcao <597895798@qq.com>
2018-05-13 22:54:37 +0800

    baseservice baseui jenkins file

--- baseservice_Jenkinsfile.groovy
+++ baseservice_Jenkinsfile.groovy
@@ -12,13 +12,13 @@ node {
     stage('maven') {
 
         sh 'echo $PWD'
-        sh 'docker run --rm --name taozhumall  -v "/opt/data/jenkins_home/workspace/${JOB_NAME}":/usr/src/mymaven -v /opt/data/maven/.m2/:/root/.m2/ -w /usr/src/mymaven maven:3.5.2-jdk-8-alpine mvn clean install -f ${moduleName}/${pomName}'
+        sh 'docker run --rm --name taozhumall  -v "/opt/data/jenkins_home/workspace/${JOB_NAME}":/usr/src/mymaven -v /opt/data/maven/.m2/:/root/.m2/ -w /usr/src/mymaven maven:3.5.2-jdk-8-alpine mvn clean install -f '+moduleName+'/'+pomName
 
     }
 
     stage('dockerbuild') {
 
-        customImage = docker.build('${moduleName}:0.1', '${workdir}/${moduleName}')
+        customImage = docker.build(moduleName+':0.1', '${workdir}/'+moduleName)
     }
 
     stage('dockerpush') {
