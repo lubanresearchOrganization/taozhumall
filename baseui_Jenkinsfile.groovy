3ed8f5e7dc9b6c947f32f620316e13d6ed09f56e
hilbertcao <597895798@qq.com>
2018-05-14 00:07:52 +0800

    jenkins file简化

--- baseui_Jenkinsfile.groovy
+++ baseui_Jenkinsfile.groovy
@@ -7,15 +7,20 @@ node {
     def mountBuildDir = "/opt/data/jenkins_home/workspace/${JOB_NAME}"
     def mountMvnDir = "/opt/data/maven/.m2/"
 
+    stage('checkout') {
+
+        checkout scm
+    }
+
     stage('maven') {
 
-        sh 'docker run --rm --name maven  -v '+mountBuildDir+':/mvn_build -v '+mountMvnDir+':/root/.m2/ -w /mvn_build maven:3.5.2-jdk-8-alpine mvn clean install -f '+moduleName+'/'+pomName
+        sh 'docker run --rm --name maven  -v ' + mountBuildDir + ':/mvn_build -v ' + mountMvnDir + ':/root/.m2/ -w /mvn_build maven:3.5.2-jdk-8-alpine mvn clean install -f ' + moduleName + '/' + pomName
 
     }
 
     stage('dockerbuild') {
 
-        dockerImage = docker.build(moduleName+':'+dockerVersion, moduleName)
+        dockerImage = docker.build(moduleName + ':' + dockerVersion, moduleName)
     }
 
     stage('dockerpush') {
