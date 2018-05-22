5e03c7b9f1acda9db69678d45099691e1fe0550b
hilbertcao <597895798@qq.com>
2018-05-23 06:57:24 +0800

    同时构建多个

--- buildAll.groovy
+++ buildAll.groovy
@@ -17,9 +17,102 @@ pipeline {
                     }
                 }
 
-                stage ('baseui') {
+                stage ('register') {
                     steps {
-                        build job: 'baseservice', parameters: [
+                        build job: 'register', parameters: [
+                        ]
+                    }
+                }
+
+                stage ('userservice') {
+                    steps {
+                        build job: 'userservice', parameters: [
+                        ]
+                    }
+                }
+
+                stage ('catagoryservice') {
+                    steps {
+                        build job: 'catagoryservice', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('commentservice') {
+                    steps {
+                        build job: 'commentservice', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('searchservice') {
+                    steps {
+                        build job: 'searchservice', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('merchantservice') {
+                    steps {
+                        build job: 'merchantservice', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('orderservice') {
+                    steps {
+                        build job: 'orderservice', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('cart') {
+                    steps {
+                        build job: 'cart', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('usercenter') {
+                    steps {
+                        build job: 'usercenter', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('platformui') {
+                    steps {
+                        build job: 'platformui', parameters: [
+                        ]
+                    }
+                }
+
+                stage ('shopui') {
+                    steps {
+                        build job: 'shopui', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('customerui') {
+                    steps {
+                        build job: 'customerui', parameters: [
+                        ]
+                    }
+                }
+
+
+                stage ('nginxserver') {
+                    steps {
+                        build job: 'nginxserver', parameters: [
                         ]
                     }
                 }
