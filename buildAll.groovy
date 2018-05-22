5bad312dc54ede76381acbaf79c94876e0f6e9c9
hilbertcao <597895798@qq.com>
2018-05-23 06:45:49 +0800

    同时构建多个

--- buildAll.groovy
+++ buildAll.groovy
@@ -1,10 +1,11 @@
-#!/usr/bin/env groovy
-def jenkinsFile
-stage('excuting baseservice_Jenkinsfile.groovy') {
-    baseserviceJenkinsfile = load "baseservice_Jenkinsfile.groovy"
-    baseserviceJenkinsfile.start()
-}
-stage('excuting baseui_Jenkinsfile.groovy') {
-    baseuiJenkinsfile = load "baseui_Jenkinsfile.groovy"
-    baseuiJenkinsfile.start();
+node {
+    stage('excuting baseservice_Jenkinsfile.groovy') {
+        baseserviceJenkinsfile = load "baseservice_Jenkinsfile.groovy"
+        baseserviceJenkinsfile.start()
+    }
+    stage('excuting baseui_Jenkinsfile.groovy') {
+        baseuiJenkinsfile = load "baseui_Jenkinsfile.groovy"
+        baseuiJenkinsfile.start();
+    }
+
 }
