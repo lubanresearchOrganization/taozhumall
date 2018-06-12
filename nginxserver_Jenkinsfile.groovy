88bab80ab5c69ca9aec57371628bbf342c024ccc
hilbertcao <597895798@qq.com>
2018-06-12 22:00:57 +0800

    nginx build update

--- nginxserver_Jenkinsfile.groovy
+++ nginxserver_Jenkinsfile.groovy
@@ -22,4 +22,9 @@ node {
         }
     }
 
+    stage('update'){
+
+        sh 'docker service update --force --image=registry.lubanresearch.com:5000/'+moduleName+':'+dockerVersion+' taozhumall_'+moduleName+' '
+    }
+
 }
