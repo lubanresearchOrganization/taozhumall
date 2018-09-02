c04e4fe02e288738f481cda779bc180885801a17
hilbertcao <597895798@qq.com>
2018-09-03 01:41:17 +0800

    分类改名

--- categoryservice/src/main/java/com/lubanresearch/lubanmall/categoryservice/Application.java
+++ categoryservice/src/main/java/com/lubanresearch/lubanmall/categoryservice/Application.java
@@ -14,8 +14,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients
 @SpringBootApplication
-@ComponentScan(basePackages={"com.lubanresearch.lubanmall.category"})
-@MapperScan("com.lubanresearch.lubanmall.category.infrastructure.persistence.db")
+@ComponentScan(basePackages={"com.lubanresearch.lubanmall.categoryservice"})
+@MapperScan("com.lubanresearch.lubanmall.categoryservice.infrastructure.persistence.db")
 @EnableAutoConfiguration(exclude = HibernateJpaAutoConfiguration.class)
 public class Application {
     public static void main(String[] args) {
